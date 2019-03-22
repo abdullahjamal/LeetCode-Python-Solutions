fc3172e7a42d81627d7782b1768b497fb2776665
Abdullah Jamal <abdullahjml11@gmail.com>
2019-03-22 04:16:05 -0400

    Update RemovedupfromSortedArr.py

--- RemovedupfromSortedArr.py
+++ RemovedupfromSortedArr.py
@@ -15,3 +15,19 @@ class Solution(object):
                 nums[count] = nums[i]
                 
         return count + 1
+
+        '''
+        if len(nums) < 1:
+            return len(nums)
+        
+        i = 1
+        
+        while (i < len(nums)):
+            
+            if nums[i] == nums[i-1]:
+                nums.pop(i)
+            else:
+                i+=1
+                
+        return len(nums)
+        '''
